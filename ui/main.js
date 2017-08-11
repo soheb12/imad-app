5cef29e42644b05d55a46efe2aecad80ea6eff38
Bigman <moinsoheb02@gmail.com>
2017-08-11 23:41:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    moveLeft = moveLeft + 10;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function()
