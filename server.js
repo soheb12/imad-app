d986236148b9063ed92736be49f33252ed94a3d0
Bigman <moinsoheb02@gmail.com>
2017-08-11 22:08:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne =
+{
+    title : 'Article One | Soheb Moin',
+    heading :'Article One',
+    date : 'AUG 8, 2017',
+    content : `<P>
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One 
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One .
+                    This Is The Content Of Article One .
+                </P>
+                
+                <P>
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One 
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One .
+                    This Is The Content Of Article One .
+                </P>
+                
+                <P>
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One 
+                    This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One . This Is The Content Of Article One .
+                    This Is The Content Of Article One .
+                </P>`
+};
+function createTemplate(data){
+    var title  = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemplate = `<html>
+    <head>
+        <title>
+            Article One | Soheb Moin 
+        </title>
+        <meta name="viewport" content="width=device-width , initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+            <div class="container">   
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                
+                <hr/>
+                
+                <h3>${heading}</h3>
+                
+                <div>
+                    ${date}
+                </div>
+                ${content}
+                </div>
+    </body>
+</html> `;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one' , function (req , res) {
- res.sendfile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two' , function (req , res) {
