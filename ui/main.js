32c72dcffc654d5b8f4924dcce0f96fe693bf24b
Bigman <moinsoheb02@gmail.com>
2017-08-11 23:40:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 
 var img = document.getElementById('madi');
 
+var marginLeft=0;
+function moveRight()
+{
+    moveLeft = moveLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function()
 {
-  img.style.marginLeft = '100px';  
+  //img.style.marginLeft = '100px';  
+    var interval = setInterval(moveRight,100);
 };
