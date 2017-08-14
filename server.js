80673d04aa82620609a3329c8050bcbe1262530a
Bigman <moinsoheb02@gmail.com>
2017-08-14 18:55:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
 var names = [];
 app.get('/submit-name/:name' , function(req , res){
    //get the name from the request
-   var name = req.query.name;
+   var name = req.params.name;
    
    names.push(name);
    res.send(JSON.stringify(names));
