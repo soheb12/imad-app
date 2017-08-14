486920eda052df0e83673ffcb6357ada679bfd85
Bigman <moinsoheb02@gmail.com>
2017-08-14 16:23:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 var button = document.getElementById('counter');
 
 
-button.onClick = function()
+button.onclick = function()
 {
     //create a request object
     var request = new XMLHttpRequest();
