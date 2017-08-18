3129a722fb6dc61f548f4b153669a7ac916dc92a
Bigman <moinsoheb02@gmail.com>
2017-08-18 18:52:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 
 var config = {
     user :'moinsoheb02',
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//var pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db' , function (req , res) {
     //make a select request
     //return a response with the results
