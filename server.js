18bc33adcbb1d71c40985c117f7a70dc71f44143
Bigman <moinsoheb02@gmail.com>
2017-08-18 18:47:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var config = {
+    user :'moinsoheb02',
+    host : 'db.imad.hasura-app.io',
+    database : 'moinsoheb02',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
    
@@ -72,9 +80,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new pool(config);
 app.get('/test-db' , function (req , res) {
     //make a select request
     //return a response with the results
+    pool.query('select * from test' , function (err , result) {
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 var names = [];
 app.get('/submit-name' , function(req , res){
