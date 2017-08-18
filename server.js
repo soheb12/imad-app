02c69ea8708edeeb23f46bb78e12b77c9300c15e
Bigman <moinsoheb02@gmail.com>
2017-08-18 18:50:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,18 +80,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pool(config);
+//var pool = new pool(config);
 app.get('/test-db' , function (req , res) {
     //make a select request
     //return a response with the results
-    pool.query('SELECT * FROM test' , function (err , result) {
+    /*pool.query('SELECT * FROM test' , function (err , result) {
        if(err){
            res.status(500).send(err.toString());
        } 
        else{
            res.send(JSON.stringify(result));
        }
-    });
+    });*/
 });
 var names = [];
 app.get('/submit-name' , function(req , res){
