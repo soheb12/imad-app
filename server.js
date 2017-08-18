6ec611470f3c2687be674912b3f817c1fe270073
Bigman <moinsoheb02@gmail.com>
2017-08-18 21:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,8 @@ counter = counter +1 ;
 });
 app.get('/articles/:articleName' , function (req , res) {
 
-   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err , result) {
+  // pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err , result) {
+     pool.query("SELECT * FROM article WHERE title = $1" ,[ req.params.articleName ] , function (err , result) {
        if(err)
        res.status(500).send(err.toString());
        else
